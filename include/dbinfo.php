6a86078a5c97bac3ea1d6bf9a512e10c671e1cba
kurewe <kurewe@gmail.com>
2017-09-08 11:28:39 -0500

    Add files via upload

---
+++ include/dbinfo.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ *
+ *  External Time Until Restart Clock/Timer
+ *  Created for Arma 3 Exile Servers
+ *
+ *  Created by: Kurewe
+ *  Website: http://cantankerousoldgoats.enjin.com/
+ *
+ */
+
+//Exile Database Login Information
+$serveraddr		= "db_serveraddr";		//exile database server address (IP address or possibly localhost)
+$username	= "db_username";	//exile database username
+$password	= "db_password";	//exile database password
+$dbname		= "db_name";			//exile database name
+
+?>
